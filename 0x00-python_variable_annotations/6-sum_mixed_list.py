945171f12acd69d48fbbdf0c7ab1d418c8a47ee6
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-19 00:13:50 +0000

    0x00. Python "Variable Annotations" - Mandatory tasks 6 - Handling a stupid checker

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''This function takes a mixed list of floats and integers and
     returns their sum'''
     return sum(mxd_lst)
