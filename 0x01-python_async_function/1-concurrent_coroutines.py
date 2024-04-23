44ad050c7d21ebe30d64f3e5542adb7d7ba61c73
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-23 02:02:21 +0000

    0x01. Python "Async" - Fixing annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 '''This script contains the definition for the function "wait_n"'''
 import asyncio
+from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     '''This asynchronous function takes two integers <n> and <max_delay>,
     and calls wait_random() <n> times with <max_delay> as input parameter'''
     delays: [float] = []
