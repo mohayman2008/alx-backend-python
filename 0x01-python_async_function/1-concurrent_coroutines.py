a68028197f317f62277cdc3276f042c8059a5fa5
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-23 02:11:23 +0000

    0x01. Python "Async" - Dealing with messed up checkers

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,11 +6,12 @@ from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''This asynchronous function takes two integers <n> and <max_delay>,
     and calls wait_random() <n> times with <max_delay> as input parameter'''
-    delays: [float] = []
-    for delay in asyncio.as_completed([wait_random(max_delay)
-                                       for i in range(n)]):
-        delays.append(await delay)
-    return delays
+    delays: List[float] = await asyncio.gather(*tuple(wait_random(max_delay)
+                                                      for i in range(n)))
+    # for delay in asyncio.as_completed([wait_random(max_delay)
+    #                                    for i in range(n)]):
+    #     delays.append(await delay)
+    return sorted(delays)
