7d94a61d182b96a214597849ebaf371f7f96655f
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-13 22:49:40 +0000

    0x03. Unittests and Integration Tests - Mandatory task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,13 @@
 '''This module contains unittest test cases for functions in "utils.py"'''
 from typing import Any, Mapping, Sequence
 import unittest
+from unittest.mock import patch
 
 from parameterized import parameterized  # type: ignore
+from requests import Response
 
-from utils import access_nested_map
+from utils import *
+# patch = unittest.mock.patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -33,3 +36,35 @@ class TestAccessNestedMap(unittest.TestCase):
         exception with the right exception messages'''
         with self.assertRaises(KeyError, msg=expected_msg):
             access_nested_map(nested_map, path)
+
+
+class MockResponse():
+    '''Class to mock the Response() object returned by requests.get(url)'''
+
+    def __init__(self, expected_payload=None, *args, **kwargs):
+        self.expected_payload = expected_payload
+
+    def json(self):
+        '''Mock of the json() method to return the "expected_payload"
+        as the object represented by the received JSON'''
+        return self.expected_payload
+
+
+class TestGetJson(unittest.TestCase):
+    '''TestCase for "utils.get_json"'''
+
+    """
+    test_url="http://example.com", test_payload={"payload": True}
+    test_url="http://holberton.io", test_payload={"payload": False}
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, expected_payload: Mapping) -> None:
+        '''Testing that function "utils.get_json" returns the expected result
+        '''
+        with patch("utils.requests.get",
+                   return_value=MockResponse(expected_payload)) as mock:
+            self.assertEqual(get_json(url), expected_payload)
+        mock.assert_called_once()
