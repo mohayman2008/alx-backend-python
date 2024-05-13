e5fde123154cc822aba4257af3f7a03586c9e3ca
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-13 09:11:19 +0000

    0x03. Unittests and Integration Tests - Mandatory task 0 - Generic, more accurate types annotations

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 '''This module contains unittest test cases for functions in "utils.py"'''
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Mapping, Sequence
 import unittest
 
 from parameterized import parameterized  # type: ignore
-# parameterized = __import__("parameterized").parameterized
 
 from utils import access_nested_map
-# access_nested_map = __import__("utils").access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -18,8 +16,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
-                               expected: Union[Dict, int]) -> None:
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
+                               expected: Any) -> None:
         '''Testing that function "utils.access_nested_map" returns the
         expected output'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -28,7 +26,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "a"),
         ({"a": 1}, ("a", "b"), "b")
     ])
-    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple,
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping, path: Sequence,
                                          expected_msg) -> None:
         '''Testing that function "utils.access_nested_map" raises the expected
         exception with the right exception messages'''
