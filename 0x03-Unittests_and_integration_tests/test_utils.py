ce5c265422d6057f24a85225bb2582249d632a30
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-13 08:19:35 +0000

    0x03. Unittests and Integration Tests - Mandatory tasks 0 & 1 - Fixing the unknown #1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,11 +3,11 @@
 from typing import Any, Mapping, Sequence
 import unittest
 
-from utils import access_nested_map
-# access_nested_map = __import__("utils").access_nested_map
-
 # from parameterized import parameterized  # type: ignore
 parameterized = __import__("parameterized").parameterized
+
+# from utils import access_nested_map
+access_nested_map = __import__("utils").access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
